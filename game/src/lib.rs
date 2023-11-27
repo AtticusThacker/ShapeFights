23cdbd5dec9822d9cab0c8b2000552540564f83b
Atticus Thacker <atticus.thacker@gmail.com>
2023-11-27 18:00:42 -0500

    start writing controller test, we'll finish later

--- game/src/lib.rs
+++ game/src/lib.rs
@@ -18,13 +18,40 @@ use fyrox::{
     scene::{
         dim2::{rectangle::Rectangle, rigidbody::RigidBody},
         node::{Node},
-        Scene, SceneLoader,
+        Scene, SceneLoader, SceneContainer,
     },
     script::{ScriptContext, ScriptTrait},
     
 };
 use std::path::Path;
 use gilrs as g;
+
+
+fn create_cube_rigid_body(graph: &mut Graph) -> Handle<Node> {
+    RigidBodyBuilder::new(BaseBuilder::new().with_children(&[
+            // Rigid body must have at least one collider
+            ColliderBuilder::new(BaseBuilder::new())
+                .with_shape(ColliderShape::cuboid(0.5, 0.5, 0.5))
+                .build(graph),
+        ]))
+    .with_mass(2.0)
+    .with_lin_vel(Vector2::new(0.0, 0.0))
+    .build(graph)
+}
+
+fn create_rect(graph: &mut Graph, resource_manager: ResourceManager) -> Handle<Node> {
+    RectangleBuilder::new(
+        BaseBuilder::new().with_local_transform(
+            TransformBuilder::new()
+                // Size of the rectangle is defined only by scale.
+                .with_local_scale(Vector3::new(0.4, 0.2, 1.0))
+                .build(),
+        ),
+    )
+    .with_color(Color::RED)
+    .with_texture(resource_manager.request::<Texture, _>("path/to/your_texture.jpg"))
+    .build(graph)
+}
 
 pub struct GameConstructor;
 
@@ -40,6 +67,8 @@ impl PluginConstructor for GameConstructor {
 
 pub struct Game {
     scene: Handle<Scene>,
+    gils: Gilrs,
+    players: HashMap<g.GamepadId, &Handle<Node>>
 }
 
 impl Game {
@@ -50,6 +79,7 @@ impl Game {
 
         Self {
             scene: Handle::NONE,
+            gils: Gilrs::new().unwrap();
         }
     }
 }
@@ -61,6 +91,33 @@ impl Plugin for Game {
 
     fn update(&mut self, _context: &mut PluginContext) {
         // Add your global update code here.
+
+        //read in all new gilrs events
+        while let Some(Event { id, event, time }) = gilrs.next_event() {
+            
+            match event.event {
+                Connected => {
+                    //create a new player
+                    //context.script.graph[handle]
+
+
+                },
+                AxisChanged(axis, value, code) => {
+                    if let Some(handle) = players.get(id){
+                        match axis {
+                            LeftStickX => // change the x velocity of the right player
+                            LeftStickY =>
+                            _ => (), //for now
+                        }
+                    }
+                },
+                _ => (), //for now
+
+            }
+
+
+            
+        }
     }
 
     fn on_os_event(
@@ -93,5 +150,37 @@ impl Plugin for Game {
         context: &mut PluginContext,
     ) {    
         self.scene = scene;
+
+    }
+}
+
+#[derive(Visit, Reflect, Debug, Clone, Default)]
+struct Player;
+
+impl_component_provider!(Player,);
+
+impl TypeUuidProvider for Player {
+    // Returns unique script id for serialization needs.
+    fn type_uuid() -> Uuid {
+        uuid!("c5671d19-9f1a-4286-8486-add4ebaadaec")
+    }
+}
+
+impl ScriptTrait for Player {
+    // Called once at initialization.
+    fn on_init(&mut self, context: &mut ScriptContext) {}
+    
+    // Put start logic - it is called when every other script is already initialized.
+    fn on_start(&mut self, context: &mut ScriptContext) { }
+
+    // Called whenever there is an event from OS (mouse click, keypress, etc.)
+    fn on_os_event(&mut self, event: &Event<()>, context: &mut ScriptContext) {}
+
+    // Called every frame at fixed rate of 60 FPS.
+    fn on_update(&mut self, context: &mut ScriptContext) {}
+
+    // Returns unique script ID for serialization needs.
+    fn id(&self) -> Uuid {
+        Self::type_uuid()
     }
 }
